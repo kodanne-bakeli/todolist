7cf24ebd0e58621a657e1a112a4a5d378476615b
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-19 13:14:53 +0000

    integrations feedbacks

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
 
+
+//fonction pour ajouter de nouvelles taches
 function Add() {
-    let search = document.getElementById("search").value
-    search.innerHTML = ""
-    return document.getElementById('taches').innerHTML +=
-        `<div class="d-flex align-items-center my-2">
-    <label class="w-75  py-2">${search}</label>
-    <input type="text" placeholder="${search}" class="form-control w-75 mx-auto">
-    <i class=" btn fa-solid fa-pen" onclick="Modify()"></i>
-    <i class="btn fa-solid fa-trash" onclick="Trash()"></i>
-    <input type="button" value="terminer" class="btn btn-success" onclick="Finish()">
+    let search = document.getElementById("search")
+    
+     
+        document.getElementById('taches').innerHTML +=
+    `<div class="d-flex align-items-center my-2 w-75 mx-auto">
+    <label class="border w-100 border-white rounded-3 p-2">${search.value}</label>
+    <input type="text" name="text" id="text" value="${search.value}" class="form-control">
+    <span class="btn btn-secondary" onclick="Enter(event)">entrer</span>
+    <i class="btn fa-solid fa-pen" onclick="Modify(event)"></i>
+    <i class="btn fa-solid fa-trash" onclick="Trash(event)"></i>
+    <i class="btn fa-solid fa-check rounded-pill btn-success" onclick="Finish(event)"></i>
     </div>
     `
+    search.value = ""
 }
-function Modify() {
-    let items = event.target.parentNode
-     items.classList.add("editMode");
-     if("Enter" === keydown){
-        items.classList.remove("editMode");
-     }
-}
-function Trash() {
-    event.target.parentNode.remove()
-}
-function Finish() {
-    event.target.parentNode.style.background = "#009900"
+//fonction pour modifier des taches
+function Modify(event) {
+     let items = event.target.parentNode
+      items.classList.add("editMode");
+    
 
 }
-// //cree une gestio de tache lutilisateur pourra ajouter afficher supprimer et modififer une tache ensuite si on termine la tache(cliquer sur terminer) le bg de la tache doit etre en vert
+function Enter(event){
+    let items = event.target.parentNode
+    let input = items.childNodes[3]
+    let label = items.childNodes[1]
+
+    label.innerText= input.value
+    
+    items.classList.remove("editMode");
+}
+//fonction pour supprimer des taches
+function Trash(event) {
+
+    event.target.parentNode.remove()
+}
+//fonction pour terminer de taches
+function Finish(event) {
+    let items = event.target.parentNode
+    let label = items.childNodes[1]
+    label.style.background = "#009900"
+
+}
