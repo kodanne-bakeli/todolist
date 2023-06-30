9d3bccc4b43a931246a79703b20525e3d7d5ea7d
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-30 20:53:11 +0000

    localstorage

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
-
 //fonction pour ajouter de nouvelles taches
+let tasks = document.getElementById('taches')
 function Add() {
-    let search = document.getElementById("search")
-    
-     
-        document.getElementById('taches').innerHTML +=
-    `<div class="d-flex align-items-center my-2 w-75 mx-auto">
+    if (search.value !== "") {
+        let search = document.getElementById("search")
+        
+        tasks.innerHTML +=
+            `<div class="d-flex align-items-center my-2 w-75 mx-auto items">
     <label class="border w-100 border-white rounded-3 p-2">${search.value}</label>
     <input type="text" name="text" id="text" value="${search.value}" class="form-control">
     <span class="btn btn-secondary" onclick="Enter(event)">entrer</span>
@@ -15,33 +14,55 @@ function Add() {
     <i class="btn fa-solid fa-check rounded-pill btn-success" onclick="Finish(event)"></i>
     </div>
     `
-    search.value = ""
+    // localStorage.setItem("tasks", tasks);
+    store();
+        search.value = ""
+        
+    } else {
+        alert(`merci d'entrer une tâche à faire`)
+    }
 }
 //fonction pour modifier des taches
 function Modify(event) {
-     let items = event.target.parentNode
-      items.classList.add("editMode");
     
-
+    let items = event.target.parentNode
+    items.classList.add("editMode");
+    store();
 }
-function Enter(event){
+function Enter(event) {
     let items = event.target.parentNode
     let input = items.childNodes[3]
     let label = items.childNodes[1]
-
-    label.innerText= input.value
-    
+    label.innerText = input.value
     items.classList.remove("editMode");
+    store();
 }
 //fonction pour supprimer des taches
 function Trash(event) {
 
     event.target.parentNode.remove()
+    store();
 }
 //fonction pour terminer de taches
 function Finish(event) {
+    
     let items = event.target.parentNode
     let label = items.childNodes[1]
     label.style.background = "#009900"
-
+    store()
 }
+//fonction permettant d'enregistrer les différentes données dans le local
+function store() {
+    window.localStorage.myitems = tasks.innerHTML;
+  }
+  //fonction permettant d'afficher les données du local dans le document 
+  function getValues() {
+    let storedValues = window.localStorage.myitems;
+    if(!storedValues) {
+        tasks
+    }
+    else {
+      tasks.innerHTML = storedValues;
+    }
+  }
+  getValues();
